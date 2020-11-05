9f2c03752901dc5b1043baa58d3d3c0e9b82a066
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2020-11-05 11:32:57 -0500

    Node Class

---
+++ AIDemoProject/Assets/Scripts/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node 
+{
+    public bool canWalk; //A boolean variable to decide if the node is walkable or not  
+    public Vector3 worldPosition; //This represents the node's point within the world  
+
+    public Node(bool canWalk_, Vector3 worldPosition_) //A constructor to assign both variables 
+    {
+        canWalk = canWalk_;
+        worldPosition = worldPosition_;
+    }
+}
